caf3a0e5b1c4a33d137e0c1176ad0da7078e795c
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2012-08-20 12:59:21 -0300

    Silver Sparrow - Logger component - first commit

---
+++ SSLoger/SSLoger/SSLoger.h
@@ -0,0 +1,23 @@
+//
+//  SSLog.h
+//  SSLog
+//
+//  Created by Ezequiel Aceto on 8/20/12.
+//  Copyright (c) 2012 Ezequiel Aceto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifdef SSLogEnabled
+
+#define SSLog_Error(message) NSLog(@"{ERROR} [%@ %@] %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), message);
+#define SSLog_Info(message) NSLog(@"{INFO} [%@ %@] %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), message);
+#define SSLog_Warn(message) NSLog(@"{WARN} [%@ %@] %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), message);
+
+#else
+
+#define SSLog_Error(message) /*Silver Sparrow - Logger disabled on Error*/
+#define SSLog_Info(message)  /*Silver Sparrow - Logger disabled on Info*/
+#define SSLog_Warn(message)  /*Silver Sparrow - Logger disabled on Warning*/
+
+#endif
